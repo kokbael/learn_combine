a1748cd1bbc2c4c3da95e0b7522cdbcc6d7eef79
kdy <kokbael@naver.com>
2025-04-07 09:37:39 +0900

    CombineDemo 프로젝트 생성

---
+++ CombineDemo/CombineDemo/CombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDemoApp.swift
+//  CombineDemo
+//
+//  Created by 김동영 on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
