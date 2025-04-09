f906edc05b5a2d81ba749ad6a36677195d96965e
kdy <kokbael@naver.com>
2025-04-09 10:27:49 +0900

    FirebaseAuth 및 FirebaseFirestore 추가: 에뮬레이터 설정 및 Firestore 설정 구성

--- CombineFirebaseBookShelf/CombineFirebaseBookShelf/CombineFirebaseBookShelfApp.swift
+++ CombineFirebaseBookShelf/CombineFirebaseBookShelf/CombineFirebaseBookShelfApp.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        // 에뮬레이터 설정
+        Auth.auth().useEmulator(withHost: "localhost", port: 9099)
+        Firestore.firestore().useEmulator(withHost: "localhost", port: 8080)
+        let settings = Firestore.firestore().settings
+        settings.cacheSettings = MemoryCacheSettings(garbageCollectorSettings: MemoryLRUGCSettings())
+        settings.isSSLEnabled = false
+        Firestore.firestore().settings = settings
+        
         return true
     }
 }
