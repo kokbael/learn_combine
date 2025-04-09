16c1d2c99ffe79fe3bb243c5be3bf6343a112bf5
kdy <kokbael@naver.com>
2025-04-09 15:56:41 +0900

    라이브 도서 목록 실습: 컴바인 ( 구독, 해지 )

--- CombineFirebaseBookShelf/CombineFirebaseBookShelf/Combines/LiveBooksListViewWithCombine.swift
+++ CombineFirebaseBookShelf/CombineFirebaseBookShelf/Combines/LiveBooksListViewWithCombine.swift
@@ -5,13 +5,54 @@
 //  Created by 김동영 on 4/9/25.
 //
 
+import Combine
 import SwiftUI
+import FirebaseFirestore
+
+private class BookListViewModel: ObservableObject {
+    @Published var books: [Book] = []
+    @Published var errorMessage: String?
+    
+    private var db = Firestore.firestore()
+    private var cancellable: AnyCancellable?
+    
+    fileprivate func subscribe() {
+        cancellable = db.collection("books").snapshotPublisher()
+            .map { querySnapshot in
+                querySnapshot.documents.compactMap { documentSnapshot in
+                    try? documentSnapshot.data(as: Book.self)
+                }
+            }
+            .catch { error in
+                self.errorMessage = error.localizedDescription
+                return Just([Book]()).eraseToAnyPublisher()
+            }
+            .replaceError(with: [Book]())
+            .handleEvents(receiveCancel: {
+                print("Cancelled")
+            })
+            .assign(to: \.books, on: self)
+    }
+    
+    fileprivate func unsubscribe() {
+        cancellable?.cancel()
+        cancellable = nil
+    }
+}
 
 struct LiveBooksListViewWithCombine: View {
+    @StateObject private var viewModel = BookListViewModel()
+    
     var body: some View {
-        VStack{
-            EmptyView()
+        List(viewModel.books) { book in
+            Text(book.title)
         }
         .navigationTitle("Book Live")
+        .onAppear {
+            viewModel.subscribe()
+        }
+        .onDisappear {
+            viewModel.unsubscribe()
+        }
     }
 }
