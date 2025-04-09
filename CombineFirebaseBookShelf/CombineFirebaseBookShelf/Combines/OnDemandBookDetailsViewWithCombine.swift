aec36483d4cc1ba93ae4a147ab580cbc4e1c7045
kdy <kokbael@naver.com>
2025-04-09 11:51:28 +0900

    온디맨드 북 세부정보 뷰 - 컴바인 타입 실습: Firestore에서 책 정보를 가져오고 오류 메시지 표시 기능 추가 및 UI 업데이트

--- CombineFirebaseBookShelf/CombineFirebaseBookShelf/Combines/OnDemandBookDetailsViewWithCombine.swift
+++ CombineFirebaseBookShelf/CombineFirebaseBookShelf/Combines/OnDemandBookDetailsViewWithCombine.swift
@@ -6,12 +6,57 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+import FirebaseFirestoreCombineSwift
+
+private class BookDetailViewModel: ObservableObject {
+    @Published var book = Book.empty
+    @Published var errorMessage: String?
+    
+    private var db = Firestore.firestore()
+    
+    fileprivate func  fetchBook() {
+        db.collection("books").document("hitchhiker").getDocument()
+            .tryMap { documentSnapshot in
+                try documentSnapshot.data(as: Book.self)
+            }
+//            .catch { error in
+//                self.errorMessage = error.localizedDescription
+//            }
+            .replaceError(with: Book.empty)
+            .assign(to: &$book)
+    }
+}
 
 struct OnDemandBookDetailsViewWithCombine: View {
+    @StateObject private var viewModel = BookDetailViewModel()
+    
     var body: some View {
-        VStack{
-            EmptyView()
+        Form {
+            Section{
+                Text(viewModel.book.title)
+                    .font(.title)
+                    .padding()
+                Text(viewModel.book.author)
+                    .font(.headline)
+                    .padding()
+                Text("\(viewModel.book.numberOfPages) pages")
+                    .font(.subheadline)
+                    .padding()
+            } footer: {
+                if let errorMessage = viewModel.errorMessage {
+                    Text(errorMessage)
+                        .foregroundColor(.red)
+                        .padding()
+                }
+            }
         }
         .navigationTitle("Book Details")
+        .onAppear {
+            viewModel.fetchBook()
+        }
+        .refreshable {
+            viewModel.fetchBook()
+        }
     }
 }
