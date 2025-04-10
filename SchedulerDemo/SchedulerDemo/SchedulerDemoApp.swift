71191b1aa33ec09fe058b228192582b306bc6503
kdy <kokbael@naver.com>
2025-04-10 09:44:54 +0900

    SchedulerDemo 프로젝트 생성

---
+++ SchedulerDemo/SchedulerDemo/SchedulerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulerDemoApp.swift
+//  SchedulerDemo
+//
+//  Created by 김동영 on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SchedulerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
