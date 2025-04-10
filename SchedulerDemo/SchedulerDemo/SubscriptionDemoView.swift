418c534649946645fbd0fff78f04bbea639ca723
kdy <kokbael@naver.com>
2025-04-10 10:34:54 +0900

    구독 및 스케줄러 뷰모델 추가: 백그라운드 작업 시작 기능 구현

---
+++ SchedulerDemo/SchedulerDemo/SubscriptionDemoView.swift
@@ -0,0 +1,33 @@
+//
+//  SubscriptionDemoView.swift
+//  SchedulerDemo
+//
+//  Created by 김동영 on 4/10/25.
+//
+
+import SwiftUI
+
+struct SubscriptionDemoView: View {
+    @StateObject var viewModel = SubscribeOnViewModel()
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Times: \(viewModel.times)")
+                .font(.headline)
+            
+            Button("Start Background Work") {
+                viewModel.start()
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Text("콘솔을 확인하여 스레드 실행 상태를 확인하세요")
+                .font(.caption)
+                .foregroundColor(.secondary)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SubscriptionDemoView()
+}
